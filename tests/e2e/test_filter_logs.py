6ef42aa06d21d35dcfbd8d08262f049b491b86f0
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-06-20 17:40:17 -0700

    empty test

---
+++ tests/e2e/test_filter_logs.py
@@ -0,0 +1,13 @@
+'''Testing team methods.'''
+import unittest
+
+from dans.endpoints.teams import Teams
+
+class TestFilterLogs(unittest.TestCase):
+    '''Tests for each method in dans.endpoints.teams'''
+    def test_filter_logs(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
