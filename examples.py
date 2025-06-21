eb5a1f8af491223b9da7f4a3444343a536a84322
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-06-21 11:09:56 -0700

    examples

--- examples.py
+++ examples.py
@@ -1,20 +1,10 @@
 '''Examples of usage.'''
 
-from dans.endpoints.playerlogs import PlayerLogs
-from dans.endpoints.playerstats import PlayerStats
-from dans.endpoints.teams import Teams
+from dans.endpoints.playbyplay.pbpplayerlogs import PBPPlayerLogs
+from dans.endpoints.playbyplay.pbpplayerstats import PBPPlayerStats
 from dans.library.arguments import DataFormat, SeasonType
 
-logs_df = PlayerLogs('LeBron James', [2012, 2014], season_type=SeasonType.playoffs).bball_ref()
-print(logs_df)
 
-stats_list = PlayerStats('Kawhi Leonard', [2019, 2021], [100, 110], \
-                            season_type=SeasonType.playoffs).nba_stats()
-print(stats_list)
+logs = PBPPlayerLogs("Kobe Bryant", "2006-07", season_type=SeasonType.playoffs).get_data_frame()
 
-stats_list = PlayerStats('Kevin Durant', [2017, 2019], [105, 110], data_format=\
-                         DataFormat.opp_pace_adj, season_type=SeasonType.playoffs).nba_stats()
-print(stats_list)
-
-teams_df = Teams([2010, 2020], [105, 110]).bball_ref()
-print(teams_df)
+print(PBPPlayerStats(logs, [80, 130]).aggregate_logs(data_format=DataFormat.opp_pace_adj))
